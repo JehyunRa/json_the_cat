fa5adbd2e0b8a0ac832f6b7416ed4096a8f84269
JehyunRa <jra4862@gmail.com>
2020-01-16 18:14:20 +0000

    b

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,10 +1,14 @@
-const argv = process.argv.slice(2);
 const request = require('request');
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${argv}`, (error, response, body) => {
-  if (!error) {
-    const data = JSON.parse(body);
-    if (data.length) console.log(`${data[0].name}: ${data[0].description}`);
-    else console.log(`data not found`);
-  } else console.error(`request failed: website not found`);
-});
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    if (error) callback(error, body);
+    else {
+      const data = JSON.parse(body);
+      if (data.length) callback(error, `${data[0].description}`);
+      else callback(error, `No contents found for ${breedName}.`);
+    }
+  });
+};
+
+module.exports = { fetchBreedDescription };
